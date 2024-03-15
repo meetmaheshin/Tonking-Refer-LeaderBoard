0d4b85cd78936ae0d5e0fa8edbfde659d9ef2f7f
meetmaheshin <63428064+meetmaheshin@users.noreply.github.com>
2024-03-15 23:00:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-fetch('http://216.48.184.198:8085/tonk/user/referral-count-data?fromDate=1710181800000&toDate=1710268199000')
+fetch('https://your-api-endpoint.com/users')
   .then(response => response.json())
   .then(data => {
+    const sortedData = data.sort((a, b) => b.referralCount - a.referralCount);
     const leaderboardBody = document.getElementById('leaderboard-body');
     leaderboardBody.innerHTML = '';
 
-    data.forEach((user, index) => {
+    sortedData.forEach((user, index) => {
       const row = document.createElement('tr');
       row.innerHTML = `
         <td>${index + 1}</td>
