678e00a6dcbca21bde63b8710e68f33257726db4
meetmaheshin <63428064+meetmaheshin@users.noreply.github.com>
2024-03-15 23:00:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,8 +8,8 @@ fetch('http://216.48.184.198:8085/tonk/user/referral-count-data?fromDate=1710181
       const row = document.createElement('tr');
       row.innerHTML = `
         <td>${index + 1}</td>
-        <td>${tgUserId}</td>
-        <td>${user.score}</td>
+        <td>${user.tgUserId}</td>
+        <td>${user.referralCount}</td>
       `;
       leaderboardBody.appendChild(row);
     });
